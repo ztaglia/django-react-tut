2196ab40e0783bf8b33cfa704fe4937995810ff6
ztaglia <ztaglia@umich.edu>
2025-11-19 15:51:50 -0600

    changed settings for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$z3f_k=^*gqhh@*4&$1)$79(af-+@s70a=kfj^$xdqj$#5-2f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
